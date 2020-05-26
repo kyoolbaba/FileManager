d93f4ea125468b7992a24a7fc7301a368d0ad74d
kyoolbaba <kyoolbaba@gmail.com>
2020-05-26 15:18:26 +0530

    Added testCases for updating contact data

--- src/test/java/com/addressBooks/AddressBookTest.java
+++ src/test/java/com/addressBooks/AddressBookTest.java
@@ -1,5 +1,7 @@
 package com.addressBooks;
 
+import com.addressbook.Person;
+import com.addressbook.SearchUpdateAndDeleteBy;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -100,9 +102,84 @@ public static final String SOURCE_LOCATION="src/test/resources/";
             fileManager.addPerson("Rahul", "kumar",
                     "HSRLayout", "12896", "Mangaore", "Goa", "4561327");
             fileManager.saveAsChangesInAddressBook("HrithikRoshan");
-            Assert.assertEquals(4,fileManager.getNoOfRecordsInTheAddressBook());
+            Assert.assertEquals(6,fileManager.getNoOfRecordsInTheAddressBook());
             File file=new File(SOURCE_LOCATION+"HrithikRoshan"+".json");
             file.delete();
+        }catch(FileManagerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenAddressBook_whenUpdatedCity_shouldReturnUpdatedCity() {
+       try{
+           FileManager fileManager = new FileManager();
+        fileManager.openAddressBook("KamallHassan");
+        fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_CITY,"Milan","London");
+        Person[] personDetails=fileManager.searchPersonDetails("Milan").toArray(new Person[0]);
+        Assert.assertEquals("London",personDetails[0].getCity());
+           fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_CITY,"Milan","Bangalore");
+           fileManager.saveChangesInAddressBook();
+    }catch(FileManagerException e){
+           e.printStackTrace();
+       }
+    }
+
+    @Test
+    public void givenAddressBook_whenUpdatedState_shouldReturnUpdatedState() {
+        try{
+            FileManager fileManager = new FileManager();
+            fileManager.openAddressBook("KamallHassan");
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_STATE,"Milan","Telangana");
+            Person[] personDetails=fileManager.searchPersonDetails("Milan").toArray(new Person[0]);
+            Assert.assertEquals("Telangana",personDetails[0].getState());
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_STATE,"Milan","Karnataka");
+            fileManager.saveChangesInAddressBook();
+        }catch(FileManagerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenAddressBook_whenUpdatedPhoneNumber_shouldReturnUpdatedPhoneNumber() {
+        try{
+            FileManager fileManager = new FileManager();
+            fileManager.openAddressBook("KamallHassan");
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_PHONENUMBER,"Milan","123123123");
+            Person[] personDetails=fileManager.searchPersonDetails("Milan").toArray(new Person[0]);
+            Assert.assertEquals("123123123",personDetails[0].getPhoneNumber());
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_PHONENUMBER,"Milan","7777777777");
+            fileManager.saveChangesInAddressBook();
+        }catch(FileManagerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenAddressBook_whenUpdatedPinCode_shouldReturnUpdatedPinCode() {
+        try{
+            FileManager fileManager = new FileManager();
+            fileManager.openAddressBook("KamallHassan");
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_PINCODE,"Milan","000000");
+            Person[] personDetails=fileManager.searchPersonDetails("Milan").toArray(new Person[0]);
+            Assert.assertEquals("000000",personDetails[0].getPincode());
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_PINCODE,"Milan","123456");
+
+        }catch(FileManagerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenAddressBook_whenUpdatedAddress_shouldReturnUpdatedAddress() {
+        try{
+            FileManager fileManager = new FileManager();
+            fileManager.openAddressBook("KamallHassan");
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_ADDRESS,"Milan","Malleshwaram");
+            Person[] personDetails=fileManager.searchPersonDetails("Milan").toArray(new Person[0]);
+            Assert.assertEquals("Malleshwaram",personDetails[0].getAddress());
+            fileManager.updateContact(SearchUpdateAndDeleteBy.UPDATE_PINCODE,"Milan","HSRLAYOUT");
+            fileManager.saveChangesInAddressBook();
         }catch(FileManagerException e){
             e.printStackTrace();
         }
